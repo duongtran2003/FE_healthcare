bb9a31fd018a3393f4d348cf0e58ecd4af8b7c62
duongtran2003 <dt005250@gmail.com>
2025-05-24 08:50:19 +0700

    scaffolding project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import MainLayout from "./layouts/MainLayout/MainLayout";
+import Dashboard from "./dashboard/Dashboard";
+import NotFound from "./notfound/NotFound";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
